f00ba61cd9b6df88f809b0d968f286d6dbf836db
Sai Revanth Kakarlapudi <sairevanth.k@ashvfinance.com>
2023-11-24 22:46:55 +0530

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.DB_STRING)
+  .then(() => {
+    console.log("DB Connection Successfully!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000, ()=> {
-    console.log(`Server is running at PORT 3000`)
-})
+app.listen(3000, () => {
+  console.log(`Server is running at PORT 3000`);
+});
