f1df8c0386332fd47a5961a466eeb7215414585e
Sai Revanth Kakarlapudi <sairevanth.k@ashvfinance.com>
2023-12-03 22:37:47 +0530

    complete update user fuctionality

--- api/middleware/validate.token.handler.js
+++ api/middleware/validate.token.handler.js
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
+import {errorHandler} from './error.handler.js'
 
 export const verifyToken = (req, res, next) => {
-  const token = req.headers.cookie.split("=")[1];
-  if (!token) return next(401, "Unauthorized");
+  const token = req?.headers?.cookie?.split("=")[1];
+  if (!token) return next(errorHandler(401, "Unauthorized"));
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(403, "Forbidden");
     req.user = user;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,9 +7,15 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase.js";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice.js";
+import  { useDispatch } from 'react-redux';
 
 export default function Profile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
@@ -18,6 +24,7 @@ export default function Profile() {
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const [showListingsError, setShowListingsError] = useState(false);
   const [userListings, setUserListings] = useState([]);
+  const dispatch = useDispatch();
   /**
    * Fire Base Storage Code.
    * 
@@ -63,10 +70,38 @@ export default function Profile() {
     );
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -96,33 +131,32 @@ export default function Profile() {
         <input
           type="text"
           placeholder="username"
-          // defaultValue={currentUser.username}
+          defaultValue={currentUser.username}
           id="username"
           className="border p-3 rounded-lg"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
-          // defaultValue={currentUser.email}
+          defaultValue={currentUser.email}
           id="email"
           className="border p-3 rounded-lg"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
-          type="text"
+          type="password"
           placeholder="password"
-          // defaultValue={currentUser.password}
+          defaultValue={currentUser.password}
           id="password"
           className="border p-3 rounded-lg"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <button
-          // disabled={loading}
+          disabled={loading}
           className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {/* {loading ? "Loading..." : "Update"} */}
-          Update
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
@@ -139,6 +173,10 @@ export default function Profile() {
           Sign out
         </span>
       </div>
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+      <p className='text-green-700 mt-5'>
+        {updateSuccess ? 'User is updated successfully!' : ''}
+      </p>
     </div>
   );
 }
