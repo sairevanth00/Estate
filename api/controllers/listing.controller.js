47f5c18c9792abfc740ccfac19875f0bc9404cf4
Sai Revanth Kakarlapudi <sairevanth.k@ashvfinance.com>
2023-12-31 12:03:54 +0530

    complete delete user listing  functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -2,10 +2,25 @@ import { errorHandler } from "../middleware/error.handler.js";
 import Listing from "../models/listing.model.js";
 
 export const createListing = async (req, res, next) => {
-    try {
-      const listing = await Listing.create(req.body);
-      res.status(201).json(listing);
-    } catch (err) {
-      next(err);
-    }
-  };
+  try {
+    const listing = await Listing.create(req.body);
+    res.status(201).json(listing);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if(!listing) 
+    return next(errorHandler(404, 'Listing not found!'))
+  if (req.user.id !== listing.userRef) 
+    return next(errorHandler(404, 'You can only delete your own listing!'))
+  
+  try {
+    await Listing.findByIdAndDelete(req.params.id)
+    res.status(200).json({message: 'Listing has been deleted!'});
+  } catch (error) {
+    next(error)
+  }
+};
