70d316b9b69aec8b9d393655ad1329400fc813b5
Sai Revanth Kakarlapudi <sairevanth.k@ashvfinance.com>
2023-12-31 12:23:05 +0530

    create update listing API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createListing, deleteListing } from "../controllers/listing.controller.js";
+import { createListing, updateListing, deleteListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../middleware/validate.token.handler.js";
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
+router.put('/update/:id', verifyToken, updateListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 
 export default router;
